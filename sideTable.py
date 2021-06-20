dc58291b763633378894c1392a72e53ac77bfe6e
KonradJuenger <76114767+KonradJuenger@users.noreply.github.com>
2021-06-20 19:02:01 +0200

    Update sideTable.py

--- sideTable.py
+++ sideTable.py
@@ -7,13 +7,13 @@ import re
 from pathlib import Path
 import ast
 import sys
-import os
+#import os
 
 
 def main():
-    print(os.path.dirname(os.path.abspath(__file__)))
+   
     in_files = get_input()
-    out_files = get_output(in_files)
+    out_files = get_output_path(in_files)
     defaults = get_defaults(out_files[0])
     ui = ini_ui(defaults)
     while True:
@@ -46,7 +46,7 @@ def get_input():
     #print(f'infiles = {file_a},{file_b},{file_c}')
     return[file_a,file_b,file_c]
 
-def get_output(in_files):
+def get_output_path(in_files):
     out_files =[]
     for file in in_files:
         if getattr(sys, 'frozen', False):
@@ -70,9 +70,12 @@ def ini_ui(default):
     sg.theme('Default1')
     icon_b64 = b"""iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAACXBIWXMAAAsTAAALEwEAmpwYAAAF8WlUWHRYTUw6Y29tLmFkb2JlLnhtcAAAAAAAPD94cGFja2V0IGJlZ2luPSLvu78iIGlkPSJXNU0wTXBDZWhpSHpyZVN6TlRjemtjOWQiPz4gPHg6eG1wbWV0YSB4bWxuczp4PSJhZG9iZTpuczptZXRhLyIgeDp4bXB0az0iQWRvYmUgWE1QIENvcmUgNi4wLWMwMDYgNzkuMTY0NzUzLCAyMDIxLzAyLzE1LTExOjUyOjEzICAgICAgICAiPiA8cmRmOlJERiB4bWxuczpyZGY9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiPiA8cmRmOkRlc2NyaXB0aW9uIHJkZjphYm91dD0iIiB4bWxuczp4bXA9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC8iIHhtbG5zOmRjPSJodHRwOi8vcHVybC5vcmcvZGMvZWxlbWVudHMvMS4xLyIgeG1sbnM6cGhvdG9zaG9wPSJodHRwOi8vbnMuYWRvYmUuY29tL3Bob3Rvc2hvcC8xLjAvIiB4bWxuczp4bXBNTT0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wL21tLyIgeG1sbnM6c3RFdnQ9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9zVHlwZS9SZXNvdXJjZUV2ZW50IyIgeG1wOkNyZWF0b3JUb29sPSJBZG9iZSBQaG90b3Nob3AgMjIuMyAoV2luZG93cykiIHhtcDpDcmVhdGVEYXRlPSIyMDIxLTA1LTA5VDEzOjMxOjI1KzAyOjAwIiB4bXA6TW9kaWZ5RGF0ZT0iMjAyMS0wNS0wOVQxMzo1MDo1MiswMjowMCIgeG1wOk1ldGFkYXRhRGF0ZT0iMjAyMS0wNS0wOVQxMzo1MDo1MiswMjowMCIgZGM6Zm9ybWF0PSJpbWFnZS9wbmciIHBob3Rvc2hvcDpDb2xvck1vZGU9IjMiIHBob3Rvc2hvcDpJQ0NQcm9maWxlPSJzUkdCIElFQzYxOTY2LTIuMSIgeG1wTU06SW5zdGFuY2VJRD0ieG1wLmlpZDo2MzljZjM1MC1hMmE4LWQyNDgtYWExYi02MTA5N2EyNDQ4NmUiIHhtcE1NOkRvY3VtZW50SUQ9ImFkb2JlOmRvY2lkOnBob3Rvc2hvcDpiNzc2YWRlMi03MmE3LTlmNDAtYjIyMy0xOTExZWNhYzE3N2UiIHhtcE1NOk9yaWdpbmFsRG9jdW1lbnRJRD0ieG1wLmRpZDoyZTI5OTFlMC0yOTIzLTI0NGUtOWRhMy1iOGFiZWViZTBmMzEiPiA8eG1wTU06SGlzdG9yeT4gPHJkZjpTZXE+IDxyZGY6bGkgc3RFdnQ6YWN0aW9uPSJjcmVhdGVkIiBzdEV2dDppbnN0YW5jZUlEPSJ4bXAuaWlkOjJlMjk5MWUwLTI5MjMtMjQ0ZS05ZGEzLWI4YWJlZWJlMGYzMSIgc3RFdnQ6d2hlbj0iMjAyMS0wNS0wOVQxMzozMToyNSswMjowMCIgc3RFdnQ6c29mdHdhcmVBZ2VudD0iQWRvYmUgUGhvdG9zaG9wIDIyLjMgKFdpbmRvd3MpIi8+IDxyZGY6bGkgc3RFdnQ6YWN0aW9uPSJzYXZlZCIgc3RFdnQ6aW5zdGFuY2VJRD0ieG1wLmlpZDo2MzljZjM1MC1hMmE4LWQyNDgtYWExYi02MTA5N2EyNDQ4NmUiIHN0RXZ0OndoZW49IjIwMjEtMDUtMDlUMTM6NTA6NTIrMDI6MDAiIHN0RXZ0OnNvZnR3YXJlQWdlbnQ9IkFkb2JlIFBob3Rvc2hvcCAyMi4zIChXaW5kb3dzKSIgc3RFdnQ6Y2hhbmdlZD0iLyIvPiA8L3JkZjpTZXE+IDwveG1wTU06SGlzdG9yeT4gPC9yZGY6RGVzY3JpcHRpb24+IDwvcmRmOlJERj4gPC94OnhtcG1ldGE+IDw/eHBhY2tldCBlbmQ9InIiPz5bS9u+AAABGUlEQVRIx+2WPQrCQBBG01gYcwQhBCKIWHsGPYA21p7ECwj2Vh7AO9jbWAjBQiEYQSLBQsUf4rcwgXXYFTZapHDgEZjMzIPNZhMrTVOLgwhALLEAZes9ypSX6wLlPI3kBFKJHagwSYXyct3JRJKw5lAjCVldYiKJwF1CLJ/NJDbl5brIROKDhoRnqcNjdb6JJHcUTuKCGi2buFY1M6uszjWRiF1zBRe6rjTvyYrVhYXbwjFr3mokW1YX/yV/STElG80pvPlGcmTNe+AwiUP53JIDa36CNpN0KJ9bsmTN2ZJ1QRP0FM/DWDJWDMh4fLhnJKmD84dhP5GI6NPxrRs4U2yQW54vYwtMwZq2tfjBmIMBKIEhmEiMVPNeISPFwVGZa4cAAAAASUVORK5CYII="""
     layout = [  [sg.Text('')],
-                [sg.Text('Temps')],
+                [sg.Text('Printer Setup')],
                 [sg.Text('   '),sg.Text('bed temperature in °C',size=(35,1)), sg.InputText(default["bed"],key='bed', justification = "right")],
                 [sg.Text('   '),sg.Text('hot-end temperature in °C',size=(35,1)), sg.InputText(default["nozzle"],key='nozzle', justification = "right")],
+                [sg.Text('   '),sg.Text('bed size X in mm ',size=(35,1)), sg.InputText(default["printer_x"],key='printer_x', justification = "right")],
+                [sg.Text('   '),sg.Text('bed size Y in mm',size=(35,1)), sg.InputText(default["printer_y"],key='printer_y', justification = "right")],
+                [sg.Text('   '),sg.Text('offset Z ',size=(35,1)), sg.InputText(default["offset_z"],key='offset_z', justification = "right")],
                 [sg.Text('G0 speeds (Travel Moves)')],
                 [sg.Text('   '),sg.Text('speed for horizontal non-print moves in mm/s',size=(35,1)), sg.InputText(default["speed_g0_h"],key='speed_g0_h', justification = "right")],
                 [sg.Text('   '),sg.Text('speed for vertical non-print moves  in mm/s',size=(35,1)), sg.InputText(default["speed_g0_v"],key='speed_g0_v', justification = "right")],
@@ -93,10 +96,13 @@ def ini_ui(default):
 
     ui = sg.Window('table gcode generator v0.1', layout,icon = icon_b64, )
     return ui
-
+    
 def generate_defaults():
     defaults = {
     'bed': '62', 
+    'printer_x': '300',
+    'printer_y': '300',
+    'offset_z': '0',
     'nozzle': '205', 
     'speed_g0_h': '150', 
     'speed_g0_v': '10', 
@@ -142,11 +148,42 @@ def gen_gcode(cfg,in_files,out_files):
                             new_line = replace_retract(line, cfg["unretraction"])
                         else: 
                             new_line = line
+                        
+                        new_line = printer_offset(new_line,cfg["printer_x"],cfg["printer_y"],cfg["offset_z"])
                         output.write(new_line)
             output.close()
     except Exception as e:
         print(e)
     
+def printer_offset(line,printer_x,printer_y,offset_z):
+    old_xyz = get_XYZ(line)
+    if old_xyz[0] == True:
+        new_X = str(round(float(old_xyz[1]) - 150.0 + (float(printer_x)/2),5))
+        new_Y = str(round(float(old_xyz[2]) - 150.0 + (float(printer_x)/2),5))
+        new_Z = str(round(float(old_xyz[3]) + float(offset_z),5))
+        
+        new_line = line.replace("X"+old_xyz[1],"X"+new_X)
+        new_line = new_line.replace("Y"+old_xyz[2],"Y"+new_X)
+        new_line = new_line.replace("Z"+old_xyz[3],"Z"+new_Z)
+        return new_line
+    else:
+        return line 
+
+def get_XYZ(line):
+    print("get xyz")
+    g1_g0 = ["G1", "G0"]
+    if any(g in line[0:4] for g in g1_g0):
+        try: 
+            pos_X = re.findall(r'(?<=\bX)-?\d+(?:\.\d+)?\b', line)[0]
+            pos_Y = re.findall(r'(?<=\bY)-?\d+(?:\.\d+)?\b', line)[0]
+            pos_Z = re.findall(r'(?<=\bZ)-?\d+(?:\.\d+)?\b', line)[0]
+            return (True, pos_X, pos_Y, pos_Z)
+        except Exception as e:
+            return (False,False,False,False)
+    else:
+        return (False,False,False,False)
+
+
 
 def get_defaults(last_gcode):
     if last_gcode.is_file(): return read_vals(last_gcode)
@@ -246,3 +283,4 @@ def numeric(value):
 
 if __name__ == "__main__":
     main()
+
